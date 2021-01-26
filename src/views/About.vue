226c72cec6c8c346cce6f8a73d3d414478f3ecfa
dss1 <123456789@qq.com>
2021-01-26 15:53:54 +0800

    vue的第一个项目

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,52 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+
+<div class="about">
+	<div id="app">
+		<button @click="reduce">-</button>
+		<span>{{num}}</span>
+		<button @click="add">+</button>
+		
+		<span>{{getnum()}}</span>
+		<span>{{getnum2}}</span>
+		
+		
+	</div>
+</div>
+
+
 </template>
+
+<script>
+	export default {
+		data(){
+			return{
+				num:10
+			}
+			
+		},
+		methods:{
+			reduce(){
+				this.num--;
+			},
+			add(){
+				this.num++;
+			},
+			getnum(){
+				console.log("普通方法执行了")
+				}
+		},
+		computed:{
+			getnum2(){
+				console.log("计算属性执行了")
+				return "计算属性执行了"+this.num
+			}
+			
+		},
+		watch:{
+			num(newvalue,oldvalue){
+				console.log("监听方法执行了",newvalue,oldvalue)
+			}
+		},
+
+	}
+</script>
